5d76862975ef7bc185704187e6ad4794899ef4c2
Khaidir Fahram <khaidir@fahram.com>
2022-01-16 08:02:53 +0700

    pass test entity new like

--- src/Domains/likes/NewLike.js
+++ src/Domains/likes/NewLike.js
@@ -1,5 +1,34 @@
 class NewReply {
+  constructor(payload) {
+    this._verifyPayload(payload);
 
+    const {
+      comment, owner,
+    } = payload;
+
+    this.comment = comment;
+    this.owner = owner;
+  }
+
+  _verifyPayload(payload) {
+    if (this._isPayloadNotContainNeededProperty(payload)) {
+      throw new Error('NEW_LIKE.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+    if (this._isPayloadNotMeetDataTypeSpecification(payload)) {
+      throw new Error('NEW_LIKE.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
+
+  _isPayloadNotContainNeededProperty({ comment, owner }) {
+    return (!comment || !owner);
+  }
+
+  _isPayloadNotMeetDataTypeSpecification({ comment, owner }) {
+    return (
+      typeof comment !== 'string'
+         || typeof owner !== 'string'
+    );
+  }
 }
 
 module.exports = NewReply;
